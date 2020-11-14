52fb14431992fbf5229578d9e615e8fa4894755f
Crimson <443971798@qq.com>
2020-11-14 22:51:41 +0800

    调整内容目录结构
    1. 调整Content的目录结构，新增Resources文件夹，从网上搜刮而来的素材都放里面，按物品种类划分目录而不是以文件类型（文件类型可以用前缀区分）
    2. 新增飞船类，为玩家的操控角色

---
+++ Source/escape_solar_system/Private/Spaceship.cpp
@@ -0,0 +1,171 @@
+﻿// Copyright 2020 H₂S. All Rights Reserved.
+
+
+#include "Spaceship.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ASpaceship::ASpaceship()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ShipMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ShipMesh"));
+
+	//ViewOrigin = CreateDefaultSubobject<USceneComponent>(TEXT("ViewOrigin"));
+	//ViewOrigin->SetupAttachment(ShipMesh);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	//SpringArm->SetupAttachment(ViewOrigin);
+	SpringArm->SetRelativeLocation(FVector(0, 0, 250));
+	SpringArm->TargetArmLength = 1000;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(SpringArm);
+}
+
+// Called when the game starts or when spawned
+void ASpaceship::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASpaceship::GravityActed_Implementation(FVector Direction, float Accel)
+{
+	ShipMesh->AddForce(Direction * Accel, NAME_None, true);
+	GEngine->AddOnScreenDebugMessage(4, 1, FColor::Green, TEXT("Gravity:") + FString::SanitizeFloat(Accel));
+}
+
+void ASpaceship::Turn(float Value)
+{
+	SpringArm->AddRelativeRotation(FRotator(0, Value, 0));
+}
+
+void ASpaceship::LookUp(float Value)
+{
+	Value = Value * -1;
+	FRotator current = SpringArm->GetRelativeRotation();
+	float target = current.Pitch + Value;
+	if (target > -89 && target < 89)
+	{
+		SpringArm->AddRelativeRotation(FRotator(Value, 0, 0));
+	}
+}
+
+void ASpaceship::MoveForward(float Value)
+{
+	if (CurEnergy <= 0) {
+		ForwardValue = 0;
+		return;
+	}
+	ForwardValue = Value;
+}
+
+void ASpaceship::MoveRight(float Value)
+{
+	if (CurEnergy <= 0) {
+		RightValue = 0;
+		return;
+	}
+	RightValue = Value;
+	const FVector ForwardVector = ShipMesh->GetForwardVector();
+	ShipMesh->AddTorqueInRadians(ForwardVector * -Value * 0.1, NAME_None, true);
+}
+
+void ASpaceship::Raise(float Value)
+{
+	if (CurEnergy <= 0) {
+		RaiseValue = 0;
+		return;
+	}
+	RaiseValue = Value;
+	const FVector RightVector = ShipMesh->GetRightVector();
+	ShipMesh->AddTorqueInRadians(RightVector * -Value * 0.1, NAME_None, true);
+}
+
+void ASpaceship::TakeOff(float Value)
+{
+	if (CurEnergy <= 0) {
+		UpValue = 0;
+		return;
+	}
+	UpValue = Value;
+}
+
+// Called every frame
+void ASpaceship::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const FVector UpVector = ShipMesh->GetUpVector();
+	const FVector ForwardVector = ShipMesh->GetForwardVector();
+	const FVector RightVector = ShipMesh->GetRightVector();
+	const float MaxPower = (UpValue != 0 && ForwardValue != 0) ? Power * 0.5 : Power;
+
+	// 向上
+	if (UpValue > 0 && UpForce < MaxPower)
+	{
+		UpForce += Power * UpValue * DeltaTime / 10;
+		GEngine->AddOnScreenDebugMessage(0, 1, FColor::Red, TEXT("UpForce:") + FString::SanitizeFloat(UpForce));
+	}
+	else if ((UpValue == 0 && UpForce > 0) || UpForce > MaxPower)
+	{
+		UpForce -= Power * DeltaTime / 10;
+		if (UpForce < 0)
+		{
+			UpForce = 0;
+		}
+		GEngine->AddOnScreenDebugMessage(0, 1, FColor::Green, TEXT("UpForce:") + FString::SanitizeFloat(UpForce));
+	}
+	
+	// 向前or向后
+	// 向后的推力只能是20%
+	const float MaxRate = ForwardValue < 0 ? 0.2 : 1;
+	if ((ForwardValue > 0 && ForwardForce < MaxPower) || (ForwardValue < 0 && ForwardForce > -MaxPower * MaxRate))
+	{
+		ForwardForce += Power * ForwardValue * DeltaTime / 10;
+		GEngine->AddOnScreenDebugMessage(1, 1, FColor::Red, TEXT("ForwardForce:") + FString::SanitizeFloat(ForwardForce));
+	}
+	else if ((ForwardValue == 0 && FMath::Abs(ForwardForce) > 0) || FMath::Abs(ForwardForce) > MaxPower * MaxRate)
+	{
+		const float ForwardUnit = ForwardForce > 0 ? 1 : -1;
+		const float DeltaForce = Power * ForwardUnit * DeltaTime / 10;
+		ForwardForce -= DeltaForce;
+		if (FMath::Abs(ForwardForce) < FMath::Abs(DeltaForce))
+		{
+			ForwardForce = 0;
+		}
+		GEngine->AddOnScreenDebugMessage(1, 1, FColor::Green, TEXT("ForwardForce:") + FString::SanitizeFloat(ForwardForce));
+	}
+	
+	float Total = FMath::Abs(UpForce) + FMath::Abs(ForwardForce);
+	if (Total > 0)
+	{
+		ShipMesh->AddForce(UpVector*UpForce + ForwardVector * ForwardForce);
+		CurEnergy -= Total * DeltaTime;
+		if (CurEnergy < 0)
+		{
+			CurEnergy = 0;
+		}
+	}
+	GEngine->AddOnScreenDebugMessage(2, 1, FColor::Green, TEXT("CurEnergy:") + FString::SanitizeFloat(CurEnergy));
+	GEngine->AddOnScreenDebugMessage(3, 1, FColor::Green, TEXT("CurPower:") + FString::SanitizeFloat(Total/Power * 100) + "%");
+}
+
+// Called to bind functionality to input
+void ASpaceship::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Turn", this, &ASpaceship::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &ASpaceship::LookUp);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASpaceship::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASpaceship::MoveRight);
+	PlayerInputComponent->BindAxis("Raise", this, &ASpaceship::Raise);
+	PlayerInputComponent->BindAxis("TakeOff", this, &ASpaceship::TakeOff);
+}
+
---
+++ Source/escape_solar_system/Public/Spaceship.h
@@ -0,0 +1,71 @@
+﻿// Copyright 2020 H₂S. All Rights Reserved.
+
+#pragma once
+
+#include "MassActorInterface.h"
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Spaceship.generated.h"
+
+UCLASS()
+class ESCAPE_SOLAR_SYSTEM_API ASpaceship : public APawn, public IMassActorInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASpaceship();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void GravityActed_Implementation(FVector Direction, float Accel) override;
+	void Turn(float Value);
+	void LookUp(float Value);
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Raise(float Value);
+	void TakeOff(float Value);
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(Category = Spaceship, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* ShipMesh = nullptr;
+
+	//UPROPERTY(Category = Spaceship, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	//class USceneComponent* ViewOrigin = nullptr;
+
+	UPROPERTY(Category = Spaceship, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* SpringArm = nullptr;
+
+	UPROPERTY(Category = Spaceship, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera = nullptr;
+
+protected:
+	UPROPERTY(Category = Spaceship, EditAnywhere, BlueprintReadWrite)
+	float Power = 0;
+
+	UPROPERTY(Category = Spaceship, EditAnywhere, BlueprintReadWrite)
+	float MaxEnergy = 0;
+
+	UPROPERTY(Category = Spaceship, EditAnywhere, BlueprintReadWrite)
+	float CurEnergy = 0;
+
+private:
+	float ForwardValue = 0;
+	float UpValue = 0;
+	float RightValue = 0;
+	float RaiseValue = 0;
+
+	float ForwardForce = 0;
+	float UpForce = 0;
+	float RightForce = 0;
+	float RaiseForce = 0;
+};
